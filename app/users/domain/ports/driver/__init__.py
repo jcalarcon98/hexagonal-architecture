48b51b4709b63a273a17a83c458e9c4d74a3c936
Jean Carlos Alarcón <jeancalarcon98@gmail.com>
2024-06-29 22:29:30 -0500

    [UPDATE] Unit tests have been updated in order to work as expected

--- app/users/domain/ports/driver/__init__.py
+++ app/users/domain/ports/driver/__init__.py
@@ -1,4 +1,3 @@
-from .user_service import UserService
 from .get_users_use_case import GetUsersUseCase
 from .create_user_use_case import CreateUserUseCase
 from .get_user_by_id_use_case import GetUserByIdUseCase
--- test/users/domain/cases/create_user_case_unit_test.py
+++ test/users/domain/cases/create_user_case_unit_test.py
@@ -9,6 +9,7 @@ from app.users.domain import User
 from app.users.domain.cases import CreateUserCase
 from app.users.domain.exceptions import UserAlreadyExists
 from app.users.domain.ports.driver import CreateUserUseCase
+from app.users.domain.ports.driver.create_user_use_case import CreateUserUseCaseRequest
 
 
 class TestCreateUserCase:
@@ -42,15 +43,16 @@ class TestCreateUserCase:
         expected_user = user_factory(identifier=random_identifier)
         create_user_case, user_repository, notification_service, id_generator = create_user_case_dependencies
         id_generator.set_id_to_generate(identifier=random_identifier)
-
-        response = create_user_case.handle(
+        create_user_case_request = CreateUserUseCaseRequest(
             name=expected_user.name,
             lastname=expected_user.lastname,
             email=expected_user.email,
             age=expected_user.age
         )
 
-        assert response == expected_user
+        response = create_user_case.handle(request=create_user_case_request)
+
+        assert response.user == expected_user
         assert user_repository.has_user_been_created(user=expected_user)
         assert notification_service.has_user_been_notified(
             message=expected_notification_message,
@@ -68,11 +70,12 @@ class TestCreateUserCase:
         create_user_case, user_repository, _, _ = create_user_case_dependencies
         user_repository.add_multiple([expected_user])
         expected_exception_message = f"The user with the given email already exists"
+        create_user_case_request = CreateUserUseCaseRequest(
+            name=expected_user.name,
+            lastname=expected_user.lastname,
+            email=expected_user.email,
+            age=expected_user.age
+        )
 
         with pytest.raises(UserAlreadyExists, match=expected_exception_message):
-            create_user_case.handle(
-                name=expected_user.name,
-                lastname=expected_user.lastname,
-                email=expected_user.email,
-                age=expected_user.age
-            )
+            create_user_case.handle(request=create_user_case_request)
